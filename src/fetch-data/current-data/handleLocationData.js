d752100e7ebe1b53b22a587d1f9f49aecd034bbd
Mostafa <mostafafinal55@gmail.com>
2024-11-14 18:34:15 +0200

    Manara JS advanced weather-app webpack fetch current data handleLocationData module

---
+++ src/fetch-data/current-data/handleLocationData.js
@@ -0,0 +1,31 @@
+import fetchAllData from "./fetchAllData.js";
+
+const data = await fetchAllData();
+
+function handleLocationData() {
+  try {
+    if (!data) {
+      throw new Error("Invalid Location From The Handle");
+    }
+    const countryName = () => data.location.country;
+    const cityName = () => data.location.name;
+    const coords = () => `${data.location.lat}, ${data.location.lon}`;
+    const region = () => data.location.region;
+    const localTime = () => data.location.localtime;
+    const sunrise = () => data.forecast.forecastday[0].astro.sunrise;
+    const sunset = () => data.forecast.forecastday[0].astro.sunset;
+    return {
+      countryName,
+      cityName,
+      coords,
+      region,
+      localTime,
+      sunrise,
+      sunset,
+    };
+  } catch (err) {
+    alert(err);
+  }
+}
+
+export { handleLocationData };
