c1017395008b51fce733027a7ad9bb2e5b25da23
Mostafa <mostafafinal55@gmail.com>
2024-11-14 20:02:08 +0200

    Manara JS advanced weather-app webpack fetch current data handleConditionData module

---
+++ src/fetch-data/current-data/handleConditionData.js
@@ -0,0 +1,28 @@
+import fetchAllData from "./fetchAllData.js";
+
+const data = await fetchAllData();
+
+function handleConditionData() {
+  try {
+    if (!data) {
+      throw new Error("Error From The API Happens In Condition Functionality");
+    }
+
+    const tempC = () => data.current.temp_c;
+    const tempF = () => data.current.temp_f;
+    const conditionText = () => data.current.condition.text;
+    const conditionIconCode = () =>
+      data.current.condition.icon.match(/(\d+\.png)/)[0];
+
+    return {
+      tempC,
+      tempF,
+      conditionText,
+      conditionIconCode,
+    };
+  } catch (err) {
+    alert(err);
+  }
+}
+
+export { handleConditionData };
