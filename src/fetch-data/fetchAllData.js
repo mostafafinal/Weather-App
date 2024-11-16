cefaaa51f28f94c5dad50832311d8c9138e8bf1d
Mostafa <mostafafinal55@gmail.com>
2024-11-16 15:00:53 +0200

    Manara JS advanced weather-app fetch-data fetchAllData module, forecast 2 days

--- src/fetch-data/fetchAllData.js
+++ src/fetch-data/fetchAllData.js
@@ -3,7 +3,7 @@ const API_KEY = "0ed88fd0e42a43e9ace201337240811";
 const fetchAllData = async (location = "cairo") => {
   try {
     const hitAPI = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=2&key=${API_KEY}`,
+      `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${API_KEY}`,
       { mode: "cors" }
     );
 
