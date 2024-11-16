6aa75bfd2556921252565b293455cd62462595aa
Mostafa <mostafafinal55@gmail.com>
2024-11-16 14:58:44 +0200

    Manara JS advanced weather-app webpack fetch future getNextDays.js module

---
+++ src/fetch-data/future-data/next-days/getNextDays.js
@@ -0,0 +1,15 @@
+import fetchAllData from "../../fetchAllData.js";
+
+const data = await fetchAllData();
+
+function getNextDays() {
+  const currDay = new Date(data.current.last_updated).getDate();
+  const nextDays = data.forecast.forecastday.filter(
+    (day) => new Date(day.date).getDate() !== currDay
+  );
+  console.log(currDay);
+
+  return nextDays;
+}
+
+export { getNextDays };
