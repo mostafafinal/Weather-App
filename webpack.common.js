2dd5a1ef3e05dceb92ee46e862538d329e3239f2
Mostafa <mostafafinal55@gmail.com>
2024-11-28 01:47:00 +0200

    Manara JS advanced weather-app add & configure copy-webpack-plugin to copy assets to the build dir with no hashing

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,29 @@
-import { resolve } from "path";
+// import { resolve } from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
+import path from "path";
+import CopyPlugin from "copy-webpack-plugin";
 
 // Get the directory name from the current module's URL
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default {
   entry: "./src/main.js",
   output: {
+    path: path.resolve("dist"),
     filename: "main.bundle.js",
-    path: resolve(__dirname, "dist"),
+    // path: resolve(__dirname, "dist"),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
     }),
   ],
   module: {
