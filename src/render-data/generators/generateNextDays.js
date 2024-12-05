99ca7c5c6d1d12a97cdf5497591eb4b2c01471f4
Mostafa <mostafafinal55@gmail.com>
2024-12-06 01:50:40 +0200

    Manara JS advanced weather-app adjusting generateNextDays' classes name generation

--- src/render-data/generators/generateNextDays.js
+++ src/render-data/generators/generateNextDays.js
@@ -1,67 +1,72 @@
 import { handleNextDays } from "../../fetch-data/future-data/next-days/handleNextDays.js";
 import { createElement } from "../../helpers/createElement.js";
 
-const days = handleNextDays();
+const nextDaysData = handleNextDays();
 
 function generateNextDays() {
   const fragment = new DocumentFragment();
-  const nextDaysCardContainer = createElement("div", {
-    class: "nextDaysContainer",
-  });
+  const nextDaysCardContainer = document.querySelector(".next-days");
 
-  days.forEach((day) => {
-    const nextDaysCard = createElement("div", { class: "nextDaysCard" });
+  nextDaysData.forEach((day) => {
+    const nextDaysCard = createElement("div", { class: "day-card" });
 
     const nextDayDate = createElement(
       "div",
-      { class: "nextDayDate" },
+      { class: "day-date" },
       `${day.getDate()}`
     );
     const nextDaysConditionIcon = createElement("img", {
-      class: "nextDays Icon",
+      class: "day-icon",
       src: `/assets/icons/${day.getConditionIcon()}.svg`,
     });
     const nextDaysConditionText = createElement(
       "div",
-      { class: "nextDaysText" },
+      { class: "day-condition" },
       `${day.getConditionText()}`
     );
 
     const nextDaysMinTemp = createElement(
       "div",
-      { class: "nextDaysMinTemp" },
-      `<i class="temp-icon">lo</i><div class="dual-value"><span class="metric"> ${day.getMinTempF()} <span class="unit">&deg;</span></span><span class="imperial"> ${day.getMinTempC()} <span class="unit">&deg;</span></span></div>`
+      { class: "detail temp-min" },
+      `<span class="detail-icon temp">lo</span>
+      <div class="dual-value">
+        <span class="metric">${day.getMinTempF()}<span class="unit">°</span></span>
+        <span class="imperial">${day.getMinTempC()}<span class="unit">°</span></span>
+      </div>`
     );
     const nextDaysMaxTemp = createElement(
       "div",
-      { class: "nextDaysMaxTemp" },
-      `<i class="temp-icon">hi</i><div class="dual-value"><span class="metric"> ${day.getMaxTempF()} <span class="unit">&deg;</span></span><span class="imperial"> ${day.getMaxTempC()} <span class="unit">&deg;</span></span></div>`
+      { class: "detail temp-max" },
+      `<span class="detail-icon temp">lo</span>
+      <div class="dual-value">
+        <span class="metric">${day.getMaxTempF()}<span class="unit">°</span></span>
+        <span class="imperial">${day.getMaxTempC()}<span class="unit">°</span></span>
+      </div>`
+    );
+    const nextDaysSnowChance = createElement(
+      "div",
+      { class: "detail percip" },
+      `<span class="detail-icon material-symbols-outlined">rainy</span>
+        <div class="detail-value">${day.getSnowChance()}</div>`
     );
     const nextDaysDetails = createElement(
       "div",
-      { class: "nextDaysDetails" },
-      `${nextDaysMinTemp.innerHTML} | ${nextDaysMaxTemp.innerHTML}`
+      { class: "day-details" },
+      `<div class="detail-divider">|</div>`
     );
-
-    const nextDaysSnowChance = createElement(
-      "div",
-      { class: "nextHoursSnowChance" },
-      `<i>rainy</i> <div class="value">${day.getSnowChance()}</div>`
-    );
+    nextDaysDetails.prepend(nextDaysMinTemp);
+    nextDaysDetails.append(nextDaysMaxTemp);
 
     nextDaysCard.append(
       nextDayDate,
       nextDaysConditionIcon,
       nextDaysConditionText,
-      nextDaysDetails,
-      nextDaysSnowChance
+      nextDaysDetails
     );
     fragment.append(nextDaysCard);
   });
 
   nextDaysCardContainer.append(fragment);
-
-  return nextDaysCardContainer;
 }
 
 export { generateNextDays };
