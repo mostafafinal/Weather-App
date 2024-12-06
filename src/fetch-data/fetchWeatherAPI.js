a91b7cd779bb88b38b7ba8fa47c8c14acbd6b9d0
Mostafa <mostafafinal55@gmail.com>
2024-12-06 13:42:07 +0200

    Manara JS advanced weather-app fix weather api handling to prevent multiple calling

---
+++ src/fetch-data/fetchWeatherAPI.js
@@ -0,0 +1,22 @@
+const API_KEY = "0ed88fd0e42a43e9ace201337240811";
+
+const fetchWeatherAPI = async (location = "cairo") => {
+  try {
+    const hitAPI = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${API_KEY}`,
+      { mode: "cors" }
+    );
+
+    if (hitAPI.status == 400) {
+      throw new Error("Invalid Location From FetchData");
+    }
+
+    const allWeatherData = await hitAPI.json();
+
+    return allWeatherData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default fetchWeatherAPI;
---
+++ src/fetch-data/handleWeatherData.js
@@ -0,0 +1,5 @@
+import fetchWeatherAPI from "./fetchWeatherAPI.js";
+
+const data = await fetchWeatherAPI();
+
+export default data;
