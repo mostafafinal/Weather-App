5cc53459684953067c4665ea7187224a4fce53c6
Mostafa <mostafafinal55@gmail.com>
2024-12-07 04:38:56 +0200

    Manara JS advanced weather-app fix background layout handler to hanlde static bg image

--- src/render-data/layout/handleBackground.js
+++ src/render-data/layout/handleBackground.js
@@ -1,21 +1,15 @@
 import { handleBackgroundData } from "../../fetch-data/current-data/handleBackgroundData.js";
-import { createElement } from "../../helpers/createElement.js";
 
-const bgData = handleBackgroundData();
+function handleBackground(data) {
+  const bgData = handleBackgroundData(data);
 
-function handleBackground() {
   const body = document.body;
-  const bodyBg = createElement("div", {
-    class: "day-night-gradient-background",
-  });
   const weatherContainerBg = document.querySelector(".bg-image img");
   const imgSrc = `assets/imgs/bg_${bgData.getBgImage()}.jpg`;
 
   weatherContainerBg.src = imgSrc;
 
   body.classList.add(`${bgData.getBgMode()}`);
-
-  body.prepend(bodyBg);
 }
 
 export { handleBackground };
