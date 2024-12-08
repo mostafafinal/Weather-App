57082193d9d84e77a0eff29a30ce5a42299a5046
Mostafa <mostafafinal55@gmail.com>
2024-12-08 16:54:09 +0200

    Manara JS advanced weather-app launch wezo app whoooooh!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
+import fetchWeatherAPI from "./fetch-data/fetchWeatherAPI.js";
+import weatherState from "./fetch-data/weatherState.js";
+import { handleHeader } from "./render-data/layout/handleHeader.js";
+import { screenController } from "./render-data/screenController.js";
+import { updateScreen } from "./render-data/updateScreen.js";
 import "./style.css";
 
+weatherState.subscribe(screenController);
+
+await fetchWeatherAPI();
+
 const refreshBtn = document.querySelector(".title-refresh");
-refreshBtn.addEventListener("click", rotateBtn);
+refreshBtn.addEventListener("click", refreshScreen);
 
 function rotateBtn() {
-  this.style.transform = "rotate(360deg)";
+  refreshBtn.style.transform = "rotate(360deg)";
+
+  setTimeout(() => (refreshBtn.style.transform = "rotate(0deg)"), 1000);
 }
+
+function refreshScreen() {
+  rotateBtn();
+
+  updateScreen();
+
+  fetchWeatherAPI(weatherState.location);
+}
+
+handleHeader();
