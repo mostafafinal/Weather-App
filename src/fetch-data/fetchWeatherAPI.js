a1d275567bc3937113f06a233c38b3d9d3876d08
Mostafa <mostafafinal55@gmail.com>
2024-12-08 17:48:50 +0200

    Manara JS advanced weather-app import page loader in the fetcher

--- src/fetch-data/fetchWeatherAPI.js
+++ src/fetch-data/fetchWeatherAPI.js
@@ -1,13 +1,18 @@
+import { hideLoader, showLoader } from "../helpers/handleLoader.js";
 import weatherData from "./weatherState.js";
 
 const fetchWeatherAPI = async (location = "cairo") => {
   const API_KEY = "0ed88fd0e42a43e9ace201337240811";
 
   try {
+    showLoader();
+
     const hitAPI = await fetch(
       `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${API_KEY}`,
       { mode: "cors" }
     );
+
+    hideLoader();
 
     if (hitAPI.status == 400) {
       throw new Error("Invalid Location");
